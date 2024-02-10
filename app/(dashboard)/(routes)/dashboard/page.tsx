9857764d02829fb1b6c1733f378c14b5ed13018d
Ben <benviscosi@gmail.com>
2024-02-10 14:15:27 -0500

    go to tool.href on click of card

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import { Card } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 
@@ -43,6 +44,8 @@ const tools = [
 ];
 
 const DashboardPage = () => {
+	const router = useRouter();
+
 	return (
 		<div>
 			<div className='mb-8 space-y-4'>
@@ -54,6 +57,7 @@ const DashboardPage = () => {
 			<div className='px-4 md:px-20 lg:px-32 space-y-4'>
 				{tools.map((tool) => (
 					<Card
+						onClick={() => router.push(tool.href)}
 						key={tool.href}
 						className='p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer'>
 						<div className='flex items-center gap-x-4'>
