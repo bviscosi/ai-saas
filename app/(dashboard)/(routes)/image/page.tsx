cf476e8b30dfa9729efa650893637766f30936fb
Ben <benviscosi@gmail.com>
2024-02-10 22:49:59 -0500

    add image src to trusted domains

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as z from 'zod';
-import { ImageIcon } from 'lucide-react';
+import { Download, ImageIcon } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
@@ -24,6 +24,8 @@ import { Empty } from '@/components/empty';
 import { Loader } from '@/components/loader';
 
 import { formSchema, amountOptions, resolutionOptions } from './constants';
+import { Card, CardFooter } from '@/components/ui/card';
+import Image from 'next/image';
 
 const ImagePage = () => {
 	const router = useRouter();
@@ -161,7 +163,24 @@ const ImagePage = () => {
 						</div>
 					)}
 					{images.length === 0 && !isLoading && <Empty label='No images generated.' />}
-					<div>Images will be rendered here</div>
+					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8'>
+						{images.map((src) => (
+							<Card key='src' className='rounded-lg overflow-hidden'>
+								<div className='relative aspect-square'>
+									<Image alt='Image' fill src={src} />
+									<CardFooter className='p-2'>
+										<Button
+											onClick={() => window.open(src)}
+											variant='secondary'
+											className='w-full'>
+											<Download className='h-4 w-4 mr-2' />
+											Download
+										</Button>
+									</CardFooter>
+								</div>
+							</Card>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
