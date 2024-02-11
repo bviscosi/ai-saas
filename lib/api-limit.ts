e219fedc1f8c1c7c172aa506ab02ee784fdf9ce7
Ben <benviscosi@gmail.com>
2024-02-11 14:01:47 -0500

    prisma config and api-limit util

---
+++ lib/api-limit.ts
@@ -0,0 +1,27 @@
+import { auth } from '@clerk/nextjs';
+
+import { MAX_FREE_COUNTS } from '@/contants';
+import prismadb from '@/lib/prismadb';
+
+export const increaseApiLimit = async () => {
+	const { userId } = auth();
+
+	if (!userId) return;
+
+	const userApiLimit = await prismadb.userApiLimit.findUnique({
+		where: {
+			userId: userId,
+		},
+	});
+
+	if (userApiLimit) {
+		await prismadb.userApiLimit.update({
+			where: { userId: userId },
+			data: { count: userApiLimit.count + 1 },
+		});
+	} else {
+		await prismadb.userApiLimit.create({
+			data: { userId: userId, count: 1 },
+		});
+	}
+};
