209df6351d4442d3990e3085fea82ba65b1edeb7
Ben <benviscosi@gmail.com>
2024-02-10 21:35:56 -0500

    Create 'code' page and api route

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as z from 'zod';
-import { MessageSquare } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
@@ -10,12 +9,14 @@ import { Input } from '@/components/ui/input';
 import { formSchema } from './constants';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
+import { Code } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
 import axios from 'axios';
 
 import { Heading } from '@/components/heading';
 import { Empty } from '@/components/empty';
 import { Loader } from '@/components/loader';
-import { cn } from '@/lib/utils';
 import { UserAvatar } from '@/components/user-avatar';
 import { BotAvatar } from '@/components/bot-avatar';
 
@@ -25,7 +26,7 @@ interface ChatCompletionRequestMessage {
 	name?: string;
 }
 
-const ConversationPage = () => {
+const CodePage = () => {
 	const router = useRouter();
 	const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
 	const form = useForm<z.infer<typeof formSchema>>({
@@ -44,7 +45,7 @@ const ConversationPage = () => {
 				content: values.prompt,
 			};
 			const newMessages = [...messages, userMessage];
-			const response = await axios.post('/api/conversation', { messages: newMessages });
+			const response = await axios.post('/api/code', { messages: newMessages });
 			setMessages((current) => [...current, userMessage, response.data]);
 			form.reset();
 		} catch (error: any) {
@@ -58,11 +59,11 @@ const ConversationPage = () => {
 	return (
 		<div>
 			<Heading
-				title='Conversation'
-				description='Our most advanced conversation model.'
-				icon={MessageSquare}
-				iconColor='text-violet-500'
-				bgColor='bg-violet-500/10'
+				title='Code Generation'
+				description='Generate code using descriptive text.'
+				icon={Code}
+				iconColor='text-green-700'
+				bgColor='bg-green-700/10'
 			/>
 			<div className='px-4 lg:px-8'>
 				<div>
@@ -88,7 +89,7 @@ const ConversationPage = () => {
 											<Input
 												className='border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent'
 												disabled={isLoading}
-												placeholder='How do I calculate the radius of a circle?'
+												placeholder='Simple toggle button using react hooks.'
 												{...field}
 											/>
 										</FormControl>
@@ -127,4 +128,4 @@ const ConversationPage = () => {
 	);
 };
 
-export default ConversationPage;
+export default CodePage;
