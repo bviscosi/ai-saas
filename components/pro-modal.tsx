05c5cd9cdd43b3112e3a5843d8ca04bb86b86320
Ben <benviscosi@gmail.com>
2024-02-24 12:59:39 -0500

    install shadcn badge, link dialog state to proModal provider

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,3 +1,18 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { useProModal } from '@/hooks/use-pro-modal';
+
 export const ProModal = () => {
-	return <div>Pro Modal</div>;
+	const proModal = useProModal();
+
+	return (
+		<Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle className='flex justify-center items-center flex-col gap-y-4 pb-2'>
+						Upgrade to Genius
+					</DialogTitle>
+				</DialogHeader>
+			</DialogContent>
+		</Dialog>
+	);
 };
