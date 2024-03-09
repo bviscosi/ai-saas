2bfa78fa4c7cabedb7834dbd7a25158d3cc4f01e
Ben <benviscosi@gmail.com>
2024-03-09 12:06:55 -0500

    hide free counter from mobile sidebar if isPro

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -30,9 +30,10 @@ const routes = [
 
 interface SidebarProps {
 	apiLimitCount: number;
+	isPro: boolean;
 }
 
-const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
+const Sidebar = ({ apiLimitCount = 0, isPro = false }: SidebarProps) => {
 	const pathname = usePathname();
 
 	return (
@@ -61,7 +62,7 @@ const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
 					))}
 				</div>
 			</div>
-			<FreeCounter apiLimitCount={apiLimitCount} />
+			<FreeCounter apiLimitCount={apiLimitCount} isPro={isPro} />
 		</div>
 	);
 };
