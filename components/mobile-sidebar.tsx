7595ca2db87e331bee45d42a71ac6e0498b342ea
Ben <benviscosi@gmail.com>
2024-03-09 21:13:04 -0500

    testing git case insensitivity

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,7 +3,7 @@
 import { Menu } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import Sidebar from '@/components/Sidebar';
+import Sidebar from '@/components/_sidebar';
 import { useEffect, useState } from 'react';
 
 interface MobileSidebarProps {
